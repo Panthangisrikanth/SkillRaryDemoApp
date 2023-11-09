cd40290260746bf27a06413f581715a79f502fbd
Panthangisrikanth <144235230+Panthangisrikanth@users.noreply.github.com>
2023-11-09 11:25:39 +0530

    Update usersPage.java

--- src/main/java/pomPages/usersPage.java
+++ src/main/java/pomPages/usersPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+//this is method class
 public class usersPage {
 //Declaration
 	@FindBy(xpath = "//h1[normalize-space(text())='Users']")
